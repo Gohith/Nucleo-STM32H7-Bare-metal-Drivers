f0f8c51725400bcaca548f9bd2616011cc493266
Gohith Kanchinatham <gohithkanchinadham3@gmail.com>
2026-01-08 22:58:34 +0100

    code restructured

--- Driver/source/GPIO.c
+++ Driver/source/GPIO.c
@@ -14,38 +14,76 @@
 
 void GPIO_Set_Mode(GPIO_register_t*, uint8_t, eGPIO_Mode_t);
 
-/**************** GLOBAL FUNCTIONS *****************/
-
-void GPIO_init(GPIO_Config_t *handle, eGPIO_Mode_t mode){
-	GPIO_Set_Mode(handle->PORT, handle->PIN, mode);
-}
-
 /**************** LOCAL FUNCTIONS *****************/
 
+/**
+*	@brief Function to set the mode of GPIO pin 
+*	@param GPIOx	Pointer to GPIO port base address
+*	@param pin 		pin number between 0 to 15
+*	@param mode		pin mode
+*	@return NONE
+*/
 void GPIO_Set_Mode(GPIO_register_t *GPIOx, uint8_t pin, eGPIO_Mode_t mode){
 	GPIOx->MODER &= ~(0x3U << (2 * pin));
 	GPIOx->MODER |= (uint32_t)mode << (2 * pin);
 }
 
 /**************** GLOBAL FUNCTIONS ************************/
+/**
+*	@brief Function to init the GPIO pin 
+*	@param handle	pointer for type GPIO_Config_t, GPIO handle
+*	@param mode		pin mode
+*	@return NONE
+*/
+void GPIO_init(GPIO_Config_t *handle, eGPIO_Mode_t mode){
+	GPIO_Set_Mode(handle->PORT, handle->PIN, mode);
+}
+
+/**
+*	@brief Function to set the GPIO pin state High
+*	@param GPIOx	Pointer to GPIO port base address
+*	@param pin 		pin number between 0 to 15
+*	@param output	pin output, either High or Low
+*	@return NONE
+*/
 void GPIO_Set_Output_High(GPIO_register_t* GPIOx, uint8_t pin,  bool output){
 	GPIOx->ODR &= ~(0x3U << pin);
 	GPIOx->ODR |= (uint16_t)output << pin;
 }
 
+/**
+*	@brief Function to set the GPIO pin state Low
+*	@param GPIOx	Pointer to GPIO port base address
+*	@param pin 		pin number between 0 to 15
+*	@return NONE
+*/
 void GPIO_Set_Output_Low(GPIO_register_t* GPIOx, uint8_t pin){
 	GPIOx->ODR &= ~(0x3U << pin);
 }
 
+/**
+*	@brief Function to get the GPIO pin state
+*	@param GPIOx	Pointer to GPIO port base address
+*	@param pin 		pin number between 0 to 15
+*	@return boolean, HIGH-pin is high, LOW-pin is low
+*/
 bool GPIO_Get_Input(GPIO_register_t* GPIOx, uint8_t pin){
-	return (GPIOx->IDR & ((uint16_t)1U << pin));
-
+	return (GPIOx->IDR & ((uint16_t)1U << pin) != 0U);
 }
 
+/**
+*	@brief Function to set the GPIO pin to alternate functionality
+*	@param GPIOx	Pointer to GPIO port base address
+*	@param pin 		pin number between 0 to 15
+*	@param value	Alternate function value
+	@return NONE
+*/
 void GPIO_Set_AFR(GPIO_register_t* GPIOx, uint8_t pin, uint8_t value){
 	if (pin < GPIO_AFRH_PIN){
+		GPIOx->AFRH &= ~(0xFU << (pin * 4));
 		GPIOx->AFRH |= (uint32_t)value << (pin * 4);
 	}else{
+		GPIOx->AFRL &= ~(0xFU << (pin * 4));
 		GPIOx->AFRL |= (uint32_t)value << (pin * 4);
 	}
 }
