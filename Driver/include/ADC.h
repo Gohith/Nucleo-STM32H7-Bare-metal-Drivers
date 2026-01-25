26108052ab056cb6141253ebe108f718446e291b
Gohith Kanchinatham <gohithkanchinadham3@gmail.com>
2026-01-26 00:55:45 +0100

    Adding ADC Driver

---
+++ Driver/include/ADC.h
@@ -0,0 +1,74 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Jan 25, 2026
+ *      Author: Gohith
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include<stdint.h>
+
+/*********************** ADC REGISTERS	*****************/
+
+typedef struct{
+	volatile uint32_t ISR;				// 0x00
+	volatile uint32_t IER;				// 0x04
+	volatile uint32_t CR;				// 0x08
+	volatile uint32_t CFGR;				// 0x0C
+	volatile uint32_t CFGR2;			// 0x10
+	volatile uint32_t SMPR1;			// 0x14
+	volatile uint32_t SMPR2;			// 0x18
+	volatile uint32_t RESERVED[5];		// RESERVED
+	volatile uint32_t SQR1;				// 0x30
+	volatile uint32_t SQR2;				// 0x34
+	volatile uint32_t SQR3;				// 0x38
+	volatile uint32_t SQR4;				// 0x3C
+	volatile uint32_t DR;				// 0x40
+}ADC_reg_t;
+
+//ADC CONFIG
+typedef struct{
+	ADC_reg_t *instance;	// ADC 1-3
+	uint8_t channel_length;
+	uint8_t *channels;
+	uint8_t *sample_times;
+	uint32_t mode;
+	uint32_t oversample;
+}ADC_config_t;
+
+//ADC MODES
+typedef enum{
+	OVERRUN_MODE	= (1U << 12),
+	CONTINUOUS_MODE	= (1U << 13),
+	DELAYED_CONVERSION_MODE	= (1U << 14),
+	DISCONTNUOUS_MODE	= (1U << 16),
+	JSQR_MODE	= (1U << 21),
+}eADC_Mode_t;
+
+//ADC OVERSAMPLE TYPE
+typedef enum{
+	ENABLE_REGULAR_OVERSAMPLE	= (1U << 0),
+	ENABLE_INJECTED_OVERSAMPLE	= (1U << 1),
+	REGULAR_OVERSAMPLE_RESUME_MODE	= (1U << 10),		// RESUME MODE FOR OVERSAMPLING, NOT CONTINUOUS
+}eOverSample_t;
+
+/*********************** ADC BASE ADDRESS	****************/
+
+#define ADC1			((volatile ADC_reg_t*)0x40022000U)		//!> ADC1 BASE ADDRESS, MASTER coupled with ADC2
+#define ADC2			((volatile ADC_reg_t*)0x40022100U)		//!> ADC2 BASE ADDRESS, SLAVE coupled with ADC1
+#define ADC3			((volatile ADC_reg_t*)0x58026000U)		//!> ADC3 INDEPENDENT MASTER BASE ADDRESS
+
+//TODO Yet to develop common registers
+#define ADC12_COMMON 	((volatile ADC_reg_t*)0x40022300U)		//!> ADC1, ADC2 COMMON REGISTER BASE ADDRESS
+#define ADC3_COMMON		((volatile ADC_reg_t*)0x58026300U)		//!> ADC3 COMMON REGISTER BASE ADDRESS
+
+
+/********************** GLOBAL FUNCTIONS	**********************/
+
+void ADC_init(ADC_config_t*);
+void ADC_Start(ADC_config_t*);
+uint16_t ADC_Read_Block(ADC_config_t*);
+
+#endif /* ADC_H_ */
---
+++ Driver/source/ADC.c
@@ -0,0 +1,116 @@
+/*
+ * ADC.c
+ *
+ *  Created on: Jan 25, 2026
+ *      Author: Gohith
+ */
+
+
+#include<ADC.h>
+
+/*************************	MACROS	**************************/
+#define RESET	((uint32_t)0x00U)
+#define SMPR1_LENGTH			9U
+#define MAX_SEQUENCE_LENGTH		16U
+
+#define SQR1_LIMIT				4U
+
+#define SQR2_START				4U
+#define SQR2_END				8U
+
+#define SQR3_START				9U
+#define SQR3_END				13U
+
+#define SQR4_START				14U
+#define SQR4_END				15U
+/************************* USER FUNCTION PROTOTYPE	******************/
+
+void ADC_Config_Channel(ADC_config_t*);
+/************************* GLOBAL FUNCTION DEFINITION	******************/
+
+/**
+ * @brief	Function to initialize ADC
+ * @param	handle	handle to ADC_config_t
+ * @return 	NONE
+ */
+void ADC_init(ADC_config_t* handle){
+	handle->instance->CR &= ~(1U << 0);
+	handle->instance->CFGR = handle->mode;
+	handle->instance->CFGR2 = handle->oversample;
+	handle->instance->ISR = RESET;
+	handle->instance->CR |= (1U << 0);
+
+	ADC_Config_Channel(handle);
+}
+
+/**
+ * @brief	Function to start the ADC
+ * @param	handle	handle to ADC_config_t
+ * @return 	NONE
+ */
+void ADC_Start(ADC_config_t* handle){
+	// EOC CLEAR
+	handle->instance->ISR |= (1U << 2);
+	//ADC START
+	handle->instance->CR |= (1U << 2);
+}
+
+/**
+ * @brief	Function to read ADC
+ * @param	handle	handle to ADC_config_t
+ * @return 	uint16_t type data
+ */
+uint16_t ADC_READ(ADC_config_t* handle){
+	while(!(handle->instance->ISR & (1U << 2))){
+		// WAIT TILL EOC
+	}
+	return (((uint16_t)handle->instance->DR) & 0xFFFF);
+}
+/************************ USER FUNCTION DEFINITION	*********************/
+
+/**
+ * @brief	Function to configure the ADC channel, sequence length and sample rate
+ * @param	handle	handle to ADC_config_t
+ * @return 	NONE
+ */
+void ADC_Config_Channel(ADC_config_t* handle){
+	if(handle->channel_length > MAX_SEQUENCE_LENGTH){
+		handle->channel_length = MAX_SEQUENCE_LENGTH;
+	}
+	// SEQUENCE LENGTH
+	handle->instance->SQR1 &= ~(0xFU << 0);
+	handle->instance->SQR1 |= ((handle->channel_length) << 0);
+
+	// CLEAR SQRx
+	handle->instance->SQR1 &= ~((0x1FU << 6) | (0x1FU << 12) | (0x1FU << 18) | (0x1FU << 24));
+	handle->instance->SQR2 &= ~((0x1FU << 0) | (0x1FU << 6) | (0x1FU << 12) | (0x1FU << 18));
+	handle->instance->SQR3 &= ~((0x1FU << 0) | (0x1FU << 6) | (0x1FU << 12) | (0x1FU << 18));
+	handle->instance->SQR4 &= ~((0x1FU << 0) | (0x1FU << 6));
+
+	for(uint8_t ui8temp = 0U; ui8temp < handle->channel_length; ui8temp++){
+		// CONFIGURE CHANNEL SEQUENCE
+		if(ui8temp < SQR1_LIMIT){
+			handle->instance->SQR1 |= (handle->channels[ui8temp] << ((ui8temp + 1U) * 6U));
+		}
+		else if(ui8temp >= SQR2_START && ui8temp <= SQR2_END){
+			handle->instance->SQR2 |= (handle->channels[ui8temp] << ((ui8temp - 4U) * 6U));
+		}
+		else if(ui8temp >= SQR3_START && ui8temp <= SQR3_END){
+			handle->instance->SQR3 |= (handle->channels[ui8temp] << ((ui8temp - 9U) * 6U));
+		}
+		else if(ui8temp >= SQR4_START && ui8temp <= SQR4_END){
+			handle->instance->SQR3 |= (handle->channels[ui8temp] << ((ui8temp - 9U) * 6U));
+		}
+
+		// CONFIGURE SAMPLE TIME FOR CHANNEL
+		if(handle->channels[ui8temp] < SMPR1_LENGTH){
+			handle->instance->SMPR1 &= ~(0x7U << (ui8temp * 3U));
+			handle->instance->SMPR1 |= (handle->sample_times[ui8temp] << (ui8temp * 3U));
+		}
+		else{
+			handle->instance->SMPR2 &= ~(0x7U << (ui8temp * 3U));
+			handle->instance->SMPR2 |= (handle->sample_times[ui8temp] << (ui8temp * 3U));
+		}
+
+	}
+}
